f61dca88824ccbfe1fa1d1aa8b02a4e208afadc4
huangwx <hwx.simle@gmail.com>
2017-09-15 21:25:23 +0800

    add ucorebook's proj1

--- src/boot/asm.h
+++ src/boot/asm.h
@@ -1,14 +1,18 @@
 #ifndef __BOOT_ASM_H__
 #define __BOOT_ASM_H__
 
-#define SEG_NULLASM     \
-    .word 0, 0;         \
+/* Assembler macros to create x86 segments */
+
+/* Normal segment */
+#define SEG_NULLASM                                             \
+    .word 0, 0;                                                 \
     .byte 0, 0, 0, 0
 
 #define SEG_ASM(type,base,lim)                                  \
     .word (((lim) >> 12) & 0xffff), ((base) & 0xffff);          \
     .byte (((base) >> 16) & 0xff), (0x90 | (type)),             \
         (0xC0 | (((lim) >> 28) & 0xf)), (((base) >> 24) & 0xff)
+
 
 /* Application segment type bits */
 #define STA_X       0x8     // Executable segment
@@ -18,4 +22,5 @@
 #define STA_R       0x2     // Readable (executable segments)
 #define STA_A       0x1     // Accessed
 
-#endif
+#endif /* !__BOOT_ASM_H__ */
+
